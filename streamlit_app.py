deefcbd0b6af01f93a451a7ab2cca1c7274ff726
Amine <49071600+Amine-Chakhchoukh@users.noreply.github.com>
2023-08-27 17:11:22 +0100

    change width of images

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if "input_form_clicked" not in st.session_state:
 if not st.session_state["input_form_clicked"]:
     with placeholder.form("input_form"):
         htp5="https://lh3.googleusercontent.com/pw/AIL4fc-PpqW403juiRdL0TilMr8xF0tXLQgiYvB7TaAktqjOfs1FIlOYwhkOudAqv4sRhXpB8dIcLp6Q9PwDbZpA-Vk10EWCYmpcP0_-980sVr5Pjt2NORzSb7976tQZ2vukEzzffuGwaf9ABz9dz36veiOfJQ=w2002-h1502-s-no"
-        st.image(htp5, width=600)
+        st.image(htp5, width=300)
         st.title("Dario!! We did it! We have an app!")
         
         response_dropdown = ('Yay!', 'Fantastic!')
@@ -35,6 +35,6 @@ if not st.session_state["input_form_clicked"]:
 if st.session_state["input_form_clicked"]:
     # with placeholder.form("new_form"):
     htp5="https://lh3.googleusercontent.com/pw/AIL4fc-u_AzKi0MTjL8-EAWJBNjO62Nlk3H5T9NTbJw0eJXsOkqDuZe7pTyhU6mWzmHMbTCnHEjU6MotCLRsc5PNGCJ8KbP83ehff-nQ6CzqIwl6J3Q8iviyIsQSKDRzfLYgLqHyCZdUmnORJKPaf4D-N-vzgw=w2002-h1502-s-no"
-    st.image(htp5, width=600)
+    st.image(htp5, width=300)
     st.write("Yaaaaay!!")
         
