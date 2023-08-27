74152fe5ef63248d50c365e078298a0b2758821e
Amine <49071600+Amine-Chakhchoukh@users.noreply.github.com>
2023-08-27 17:16:03 +0100

    add Python to the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ if not st.session_state["input_form_clicked"]:
     with placeholder.form("input_form"):
         htp5="https://lh3.googleusercontent.com/pw/AIL4fc-PpqW403juiRdL0TilMr8xF0tXLQgiYvB7TaAktqjOfs1FIlOYwhkOudAqv4sRhXpB8dIcLp6Q9PwDbZpA-Vk10EWCYmpcP0_-980sVr5Pjt2NORzSb7976tQZ2vukEzzffuGwaf9ABz9dz36veiOfJQ=w2002-h1502-s-no"
         st.image(htp5, width=300)
-        st.title("Dario!! We did it! We have an app!")
+        st.title("Dario!! We did it! We have a (Python) app!")
         
         response_dropdown = ('Yay!', 'Fantastic!')
         
